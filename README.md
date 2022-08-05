a234965b1b4ec6308d266f53705f86e230d93706
Sébastien LE BOTE <lebote.s@gmail.com>
2022-08-05 17:22:58 +0200

    Initial commit - forked bnb/bitandbang and modified for @slebote card

---
+++ README.md
@@ -0,0 +1,20 @@
+npm personnal card
+
+fork of [bnb/bitandbang](https://github.com/bnb/bitandbang) by Tierney Cyren
+
+# Usage
+
+## dev preview
+```
+npm run dev
+```
+
+## public card
+```
+npm publish
+```
+
+## consul your card
+```
+npx slebote
+```
---
+++ build.js
@@ -0,0 +1,60 @@
+'use strict'
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+const fs = require('fs')
+const path = require('path')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('           Sébastien LE BOTE'),
+  handle: chalk.white('@slebote'),
+  shorthandle: chalk.white('bnb'),
+  work: chalk.white('Senior developper at Norsys'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
+  github: chalk.gray('https://github.com/') + chalk.green('slebote'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
+  web: chalk.cyan('https://www.norsys.fr'),
+  npx: chalk.red('npx') + ' ' + chalk.white('slebote'),
+  labelWork: chalk.white.bold('    Work:'),
+  labelTwitter: chalk.white.bold(' Twitter:'),
+  labelnpm: chalk.white.bold('     npm:'),
+  labelGitHub: chalk.white.bold('  GitHub:'),
+  labelLinkedIn: chalk.white.bold('LinkedIn:'),
+  labelWeb: chalk.white.bold('     Web:'),
+  labelCard: chalk.white.bold('    Card:')
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+// const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
+const heading = `${data.name} / ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+// const twittering = `${data.labelTwitter}  ${data.twitter}`
+// const npming = `${data.labelnpm}  ${data.npm}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+// const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               // twittering + newline + // data.labelTwitter + data.twitter
+               // npming + newline + // data.labelnpm + data.npm
+               githubing + newline + // data.labelGitHub + data.github
+               // linkedining + newline + // data.labelLinkedIn + data.linkedin
+               webing + newline + newline + // data.labelWeb + data.web
+               carding // data.labelCard + data.npx
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
